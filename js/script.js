764545234b33fa567fc923d3ddfa50fc1424fa4d
eduardo <eduardoboscocs@gmail.com>
2020-08-29 11:13:20 -0300

    footer

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     //Progress bar
     let containerA = document.getElementById("circleA");
 
-    let circleA = new ProgressBar.Circle(containerA,{
+    let circleA = new ProgressBar.Circle(containerA, {
 
         color: '#64DAF9',
         strokeWidth: 6,
         duration: 1400,
-        from: {color: '#AAA'},
-        to: {color: '#65DAF9'},
+        from: { color: '#AAA' },
+        to: { color: '#65DAF9' },
 
-        step: function(state, circle){
+        step: function (state, circle) {
             circle.path.setAttribute('stroke', state.color);
-            
+
             let value = Math.round(circle.value() * 60);
 
             circle.setText(value);
@@ -25,17 +25,17 @@ $(document).ready(function(){
 
     let containerB = document.getElementById("circleB");
 
-    let circleB = new ProgressBar.Circle(containerB,{
+    let circleB = new ProgressBar.Circle(containerB, {
 
         color: '#64DAF9',
         strokeWidth: 6,
         duration: 1600,
-        from: {color: '#AAA'},
-        to: {color: '#65DAF9'},
+        from: { color: '#AAA' },
+        to: { color: '#65DAF9' },
 
-        step: function(state, circle){
+        step: function (state, circle) {
             circle.path.setAttribute('stroke', state.color);
-            
+
             let value = Math.round(circle.value() * 254);
 
             circle.setText(value);
@@ -47,17 +47,17 @@ $(document).ready(function(){
 
     let containerC = document.getElementById("circleC");
 
-    let circleC = new ProgressBar.Circle(containerC,{
+    let circleC = new ProgressBar.Circle(containerC, {
 
         color: '#64DAF9',
         strokeWidth: 6,
         duration: 1800,
-        from: {color: '#AAA'},
-        to: {color: '#65DAF9'},
+        from: { color: '#AAA' },
+        to: { color: '#65DAF9' },
 
-        step: function(state, circle){
+        step: function (state, circle) {
             circle.path.setAttribute('stroke', state.color);
-            
+
             let value = Math.round(circle.value() * 32);
 
             circle.setText(value);
@@ -69,17 +69,17 @@ $(document).ready(function(){
 
     let containerD = document.getElementById("circleD");
 
-    let circleD = new ProgressBar.Circle(containerD,{
+    let circleD = new ProgressBar.Circle(containerD, {
 
         color: '#64DAF9',
         strokeWidth: 6,
         duration: 2000,
-        from: {color: '#AAA'},
-        to: {color: '#65DAF9'},
+        from: { color: '#AAA' },
+        to: { color: '#65DAF9' },
 
-        step: function(state, circle){
+        step: function (state, circle) {
             circle.path.setAttribute('stroke', state.color);
-            
+
             let value = Math.round(circle.value() * 5243);
 
             circle.setText(value);
@@ -88,14 +88,14 @@ $(document).ready(function(){
 
 
     });
-//iniciando o loader quando o usaurio chega no elemento
+    //iniciando o loader quando o usuario chega no elemento
     let dataAreaOffset = $('#data-area').offset();
     let stop = 0;
 
-    $(window).scroll(function(e){
+    $(window).scroll(function (e) {
 
         let scroll = $(window).scrollTop();
-        if (scroll > (dataAreaOffset.top - 500) && stop == 0){
+        if (scroll > (dataAreaOffset.top - 500) && stop == 0) {
 
             circleA.animate(1.0);
             circleB.animate(1.0);
@@ -111,13 +111,99 @@ $(document).ready(function(){
     //Parallax
 
     //carregar as imagens primeiro
-    setTimeout (function(){
+    setTimeout(function () {
 
-        $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png' });
-        $('#apply-area').parallax({imageSrc: 'img/pattern.png' });
+        $('#data-area').parallax({ imageSrc: 'img/cidadeparallax.png' });
+        $('#apply-area').parallax({ imageSrc: 'img/pattern.png' });
 
     }, 250);
 
-    
+    // Filtro portfólio
 
+    $('.filter-btn').on('click', function () {
+
+        let type = $(this).attr('id');
+        let boxes = $('.project-box');
+
+        $('.main-btn').removeClass('active');
+        $(this).addClass('active');
+
+        if (type == 'dsg-btn') {
+            eachBoxes('dsg', boxes);
+        } else if (type == 'dev-btn') {
+            eachBoxes('dev', boxes);
+        } else if (type == 'seo-btn') {
+            eachBoxes('seo', boxes);
+        } else {
+            eachBoxes('all', boxes);
+        }
+
+    });
+
+    function eachBoxes(type, boxes) {
+
+        if (type == 'all') {
+            $(boxes).fadeIn();
+        } else {
+            $(boxes).each(function () {
+                if (!$(this).hasClass(type)) {
+                    $(this).fadeOut('slow');
+                } else {
+                    $(this).fadeIn();
+                }
+            });
+        }
+    }
+
+    //login
+    mobiscroll.settings = {
+    theme: 'ios',
+    themeVariant: 'light'
+};
+
+var account = mobiscroll.popup('#demo', {
+        display: 'center'
+    }),
+    popup = mobiscroll.popup('#list', {
+        display: 'center',
+        onSet: function (event, inst) {
+            var checked = document.querySelector('input[name="update"]:checked');
+            document.getElementById('showPopup').value = checked ? checked.value : '';
+        }
+    }),
+    scrollable = mobiscroll.popup('#scrollable', {
+        display: 'center',
+        scrollLock: false,
+        cssClass: 'mbsc-no-padding md-content-scroll',
+        buttons: []
+    });
+
+mobiscroll.listview('#listview', {
+    enhance: true,
+    swipe: false,
+    onItemTap: function () {
+        scrollable.hide();
+        mobiscroll.toast({
+            message: event.target.textContent + ' clicked'
+        });
+    }
+});
+
+document
+    .getElementById('showAccount')
+    .addEventListener('click', function () {
+        account.show();
+    }, false);
+
+document
+    .getElementById('showPopup')
+    .addEventListener('click', function () {
+        popup.show();
+    }, false);
+
+document
+    .getElementById('showScrollable')
+    .addEventListener('click', function () {
+        scrollable.show();
+    }, false);
 });
